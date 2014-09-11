b432a67079c0ebdb903c89a2e1d9a9eff45cf5c0
Thomas Reese <thomas@pni-dig-129096.local>
2014-09-11 14:40:11 -0700

    otherwise

--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,7 @@
 function testFunc() {
 var ol = document.getElementById("bluegold-list");
 var items = ol.getElementsByTagName("li");
-for (var i = 1; i <= items.length; ++i) {
-    if ( i % 3 === 0 ) {
-        if ( i % 5 === 0 ) { 
-            items[i-1].innerText = "BlueGold";
-        }else{
-            items[i-1].innerText = "Blue";
-        }  
-    }else {
-    if ( i % 5 === 0 ) { 
-        items[i-1].innerText = "Gold";
-    } 
+    for (var i = 1; i <= items.length; ++i) {
+        ( i % 3 === 0 ) ? (( i % 5 === 0 ) ? items[i-1].innerText = "BlueGold" : items[i-1].innerText = "Blue") : ( i % 5 === 0 ) ? items[i-1].innerText = "Gold" : void 0;
     }
-}
 }
